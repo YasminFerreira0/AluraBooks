a3871666c9f236b58bfa716c29cd9a2f1ae375ea
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-01-10 22:12:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World!");
+
+//sincrono - código executado linha por linha
+console.log("olá mundo");
+console.log("Estou aprendendo sobre sincrocidade e assincocidade no javascrip");
+console.log("event loop, call stack e task queue.");
+
+//assincrono - executa codigos em segundo plano
+
+console.log("olá mundo");
+
+function termos() {
+    console.log("event loop, call stack e task queue.");
+}
+
+setTimeout(termos, 2000);
+console.log("Estou aprendendo sobre sincrocidade e assincocidade no javascrip");
