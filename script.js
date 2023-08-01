61dd3757f9da33fd4da62f03ebcce9c07729e5a0
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-08-01 15:21:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,89 +29,35 @@ var consultaCEP = fetch('http://viacep.com.br/ws/01001000/json/')
 console.log(consultaCEP);*/
 
 
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
-        var consultaCEP = await fetch('http://viacep.com.br/ws/01001000/json/');
-        var consultaCEPConvertido = await consultaCEP.json();
-        if (consultaCEPConvertido.erro) {
-            throw Error("Cep não existente!");
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
         }
-        console.log(consultaCEPConvertido);
-    }catch (erro) {
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
         console.log(erro);
     }
 }
 
-buscaEndereco();
+/*let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas = console.log(respostas));*/
 
 
-
-
-
-
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-
-
-
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
-//commet aleatorio
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
